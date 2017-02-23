3fbbc4396a4b291af94d49b34f11f895d5761cf1
teo.mertanen <teo.mertanen@gofore.com>
2017-02-23 16:35:35 +0200

    don't async script loading

--- src/main/webapp/apply-raamit.js
+++ src/main/webapp/apply-raamit.js
@@ -42,7 +42,7 @@ function loadScript(expected, src, callback) {
     if (typeof expected == "undefined") {
         var script = document.createElement('script');
         script.type = 'text/javascript';
-        script.async = true;
+        script.async = false;
         script.onload = callback;
         script.src = src;
         document.getElementsByTagName('head')[0].appendChild(script);
