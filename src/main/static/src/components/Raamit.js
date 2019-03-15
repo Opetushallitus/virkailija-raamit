948a925a5067a2b9555dd8dc7300d7e9c067e3aa
Tatu Honkanen <tatu.honkanen@cybercom.com>
2019-03-15 14:24:27 +0200

    BUG-1890 : WIP - testing

--- src/main/static/src/components/Raamit.js
+++ src/main/static/src/components/Raamit.js
@@ -4,7 +4,6 @@ import environments from '../resources/environments.json';
 import Header from './Header';
 import SignOut from './SignOut';
 import Translations from './Translations';
-import userData from '../dev/me.json';
 import opintopolkuLogo from '../virkailija-raamit/img/opintopolkufi.png';
 import MediaQuery from 'react-responsive';
 import Icon from './Icon/Icon';
@@ -67,6 +66,9 @@ export default class Raamit extends React.Component {
     async getUserData(){
 
         try {
+            const casResponse = await fetch(urls["cas.prequel"], {
+                credentials: 'include'
+            });
             const response = fetch(urls["cas.me"],{
                 credentials: 'include',
                 mode: 'cors',
@@ -80,39 +82,12 @@ export default class Raamit extends React.Component {
                 this.getTranslate();
             }
         } catch (error) {
-            try {
                 console.log(error);
-                console.log("calling prequel")
-                const casResponse = await fetch(urls["cas.prequel"], {
-                    credentials: 'include',
-                    mode: 'cors'
-                });
-                console.log("CAS SESSIO VANHENTUNUT, yrtetään uudelleen");
-                const response = fetch(urls["cas.me"], {
-                    credentials: 'include',
-                    mode: 'cors',
-                });
-                const ud = response.json();
-                this.setState({
-                    userData: ud
-                });
-                if (ud) {
-                    window.myroles = ud.groups;
-                    this.getTranslate();
-                }
-            } catch (error) {
-                if (window.location.host.indexOf('localhost') === 0 || window.location.host.indexOf('10.0.2.2') === 0) { // dev mode (copypaste from upper)
-                    this.setState({userData});
-                    if (userData) {
-                        this.getTranslate();
-                    }
-                } else { // real usage
-                    if (window.location.href.indexOf('ticket=') > 0) { // to prevent strange cyclic cas login problems (atm related to sticky sessions)
-                        alert('Problems with login, please reload page or log out and try again');
-                    } else {
-                        // window.location.href = urls["cas.login"] + window.location.href;
-                        window.location.href = urls["cas.login"] + 'https://' + window.location.host + urls["virkailijan-stp-ui.etusivu"];
-                    }
+                if (window.location.href.indexOf('ticket=') > 0) { // to prevent strange cyclic cas login problems (atm related to sticky sessions)
+                    alert('Problems with login, please reload page or log out and try again');
+                } else {
+                    // window.location.href = urls["cas.login"] + window.location.href;
+                    window.location.href = urls["cas.login"] + 'https://' + window.location.host + urls["virkailijan-stp-ui.etusivu"];
                 }
             }
         }
