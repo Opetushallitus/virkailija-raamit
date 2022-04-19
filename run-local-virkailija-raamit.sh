f6265006db187edc9a0dbd323cda9687f25324d5
Aleksi Huotala <aleksi.huotala@reaktor.com>
2022-04-19 10:34:12 +0300

    Lisää nonce script-tägeihin
    Lisää bash-skripti virkailija-raamit hostaamiseen lokaalisti. Lisää addInlineStyle-funktio inline-tyylitiedostojen lisäämiseksi

--- run-local-virkailija-raamit.sh
+++ run-local-virkailija-raamit.sh
@@ -2,4 +2,4 @@
 cd src/main/static && npm run build
 cd ../../../
 docker build . -t virkailija-raamit-nginx
-docker run -p 8080:80 -it virkailija-raamit-nginx
+docker run -p 8080:80 --rm --name virkailija-raamit-nginx-local -it virkailija-raamit-nginx
